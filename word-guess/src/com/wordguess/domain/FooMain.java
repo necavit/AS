e1c9aef782b3248864aa7a7bbf85c8ecc8c3a5f6
David Martínez <gollumdeagol@gmail.com>
2014-04-16 17:18:55 +0200

    first project commit and some gitignore rules added

---
+++ word-guess/src/com/wordguess/domain/FooMain.java
@@ -0,0 +1,13 @@
+package com.wordguess.domain;
+
+public class FooMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ word-guess/src/com/wordguess/domain/model/Administrator.java
@@ -0,0 +1,15 @@
+package com.wordguess.domain.model;
+
+public class Administrator extends RegisteredUser {
+
+	private String telephone;
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+}
