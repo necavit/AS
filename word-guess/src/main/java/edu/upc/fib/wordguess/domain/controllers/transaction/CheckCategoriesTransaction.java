072c1123d80cd05a24dfc83f33d34ca52da3775a
miquel masriera quevedo <miquelmasriera@gmail.com>
2014-06-19 16:21:34 +0200

    categoryDAO and CategoryNotExists added

--- word-guess/src/main/java/edu/upc/fib/wordguess/domain/controllers/transaction/CheckCategoriesTransaction.java
+++ word-guess/src/main/java/edu/upc/fib/wordguess/domain/controllers/transaction/CheckCategoriesTransaction.java
@@ -1,5 +1,27 @@
 package edu.upc.fib.wordguess.domain.controllers.transaction;
 
-public class CheckCategoriesTransaction {
+import java.util.ArrayList;
+
+import edu.upc.fib.wordguess.data.DataControllersFactory;
+import edu.upc.fib.wordguess.data.dao.RegisteredUserDAO;
+
+public class CheckCategoriesTransaction implements Transaction< ArrayList<String> > {
+	
+	private ArrayList<String> categories;
+	
+	public CheckCategoriesTransaction(){
+		categories = new ArrayList<String>(); 
+	}
+	
+	@Override
+	public ArrayList<String> execute(){
+		
+		ArrayList<String> result = new ArrayList<String>();
+		//data controllers acquisition
+		DataControllersFactory dataFactory = DataControllersFactory.getInstance();
+		
+		
+		return result;
+	}
 	
 }
