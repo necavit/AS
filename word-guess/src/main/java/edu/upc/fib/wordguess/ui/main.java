3acbdeae4b45752d5d645ff83f3fa46fd44e7cfd
darinez <daniel.arinez92@gmail.com>
2014-06-19 12:46:42 +0200

    f

---
+++ word-guess/src/main/java/edu/upc/fib/wordguess/ui/main.java
@@ -0,0 +1,12 @@
+package edu.upc.fib.wordguess.ui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class main {
+    public static void main( String[] args ) {  
+    		          JFrame login = new JLogin();
+    		          login.setSize(600, 400);
+    		          login.setVisible(true);
+    }
+}
