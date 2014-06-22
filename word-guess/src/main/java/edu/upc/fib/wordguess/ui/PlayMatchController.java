d1e263097ab1b00c5549d167fc295d7ffefa1958
miquel masriera quevedo <miquelmasriera@gmail.com>
2014-06-22 16:37:37 +0200

    ui controller commented

--- word-guess/src/main/java/edu/upc/fib/wordguess/ui/PlayMatchController.java
+++ word-guess/src/main/java/edu/upc/fib/wordguess/ui/PlayMatchController.java
@@ -10,21 +10,43 @@ import edu.upc.fib.wordguess.domain.controllers.usecase.PlayLetterInfoTuple;
 import edu.upc.fib.wordguess.domain.exception.InvalidPasswordException;
 import edu.upc.fib.wordguess.domain.model.Category;
 
+/*
+ Controlador de la capa de presentacio
+ te un controlador de les vistes i el controlador de cas d'us de domini (associacions)
+ */
+
 public class PlayMatchController {
 	
 	private PlayMatchView jpv;
 	private PlayMatchUseCaseController jpuc;
 	private int idPartida;
 	
+	/**
+	 * constructora per defecte
+	 */
 	public PlayMatchController () {
 		jpuc = new PlayMatchUseCaseController();
 		jpv = new PlayMatchView(this);		
 	}
 	
+	/**
+	 *arranca la interficie grafica 
+	 */
 	public void initialize() {
 		jpv.setVisible(true);	
 	}
 				
+	/**
+	 * al premer el boto de login es comprovara si es pot loguejar
+	 * en cas contrari es capturaran les excepcions corresponent
+	 * tot i ser el login correcte aqui comprovem si el sistema disposa de categories
+	 * per passar a la seguent vista
+	 * 
+	 * 
+	 * @username
+	 * 
+	 * @pass
+	 * */
 	public boolean PrLogin(String username,String pass){
 		boolean logged = false;
 		try {
@@ -37,11 +59,9 @@ public class PlayMatchController {
 		
 		List<Category> cats = jpuc.fetchCategories();
 		jpv.loadCategories(cats);
-		//Capturar Excepcio no hi ha categories
 		if (cats.size() == 0) {
 			jpv.showMessage("No hi ha Categories", 1);
 		}
-		
 		return logged;
 	}
 	
@@ -63,11 +83,21 @@ public class PlayMatchController {
 		jpv.buildWord(jpuc.getMatchWord().length());
 	}
 	
+	/**
+	 * para la partida, retornara a la pantalla de seleccio de categories
+	 */
 	public void PrStopMatch(){
 		System.out.println("JugarPartidaController.PrAturarPartida()");
 		jpuc.stopMatch();
 	}
 	
+	/**
+	 * li passem una posicio i una lletra i el sistema comprova si es un encert
+	 * i actualitza tot el que calgui a la partida
+	 * 
+	 * @param pos
+	 * @param lletra
+	 */
 	public void PrCheck(int pos, String lletra) {
 		PlayLetterInfoTuple playInfo = jpuc.playLetter(pos, lletra.charAt(0));
 		jpv.markLetterBox(playInfo.success);
@@ -78,6 +108,9 @@ public class PlayMatchController {
 		jpv.updateErrors(playInfo.numErrors);
 	}
 		
+	/**
+	 * tanca le programa un cop s'ha acabat el joc
+	 */
 	public void PrFinishGame(){
 		System.exit(-1);
 	}
